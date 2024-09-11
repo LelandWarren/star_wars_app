a24af23d66eceb728acb23ce5327f3596e225cd3
Leland Warren <leland.a.warren@gmail.com>
2024-09-11 00:26:22 -0500

    some more fixes

--- app/controllers/star_wars_controller.rb
+++ app/controllers/star_wars_controller.rb
@@ -7,13 +7,14 @@ class StarWarsController < ApplicationController
   def films
     @films = self.class.get('/films').parsed_response["results"]
   end
-
+  
   def show_film
     film_id = params[:id]
     @film = self.class.get("/films/#{film_id}").parsed_response
-
-    render turbo_stream: turbo_stream.replace("films-frame", partial: "film_details", locals: { film: @film })
+  
+    render partial: "star_wars/film_details", locals: { film: @film }, formats: [:html]
   end
+  
 
   def people
     @people = self.class.get('/people').parsed_response["results"]
