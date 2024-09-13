dd3a2263d00d4894df3cb46f805102403a74b398
Leland Warren <leland.a.warren@gmail.com>
2024-09-12 21:07:06 -0500

    added waayyyy more caching

--- app/controllers/star_wars_controller.rb
+++ app/controllers/star_wars_controller.rb
@@ -7,22 +7,30 @@ class StarWarsController < ApplicationController
   
   def show_film
     film_id = params[:id]
-    cache_key = "film_#{film_id}"
-
-    if Rails.cache.exist?(cache_key)
-      logger.info "Cache hit for #{cache_key}"
-    else
-      logger.info "Cache miss for #{cache_key}"
-    end
-
-    # Fetch the film data, either from cache or the API
-    @film = Rails.cache.fetch(cache_key, expires_in: 12.hours) do
-      logger.info "Fetching data from API for film #{film_id}"
+    
+    @film = Rails.cache.fetch("film_#{film_id}", expires_in: 12.hours) do
       StarWarsApiService.get_film(film_id)
     end
-
+  
+    # Fetch related characters (people) and cache the full object
+    @characters = @film["characters"].map do |url|
+      character_id = url.split("/").last
+      Rails.cache.fetch("person_#{character_id}", expires_in: 12.hours) do
+        StarWarsApiService.get_person(character_id)
+      end
+    end
+  
+    # Fetch related vehicles and cache the full object
+    @vehicles = @film["vehicles"].map do |url|
+      vehicle_id = url.split("/").last
+      Rails.cache.fetch("vehicle_#{vehicle_id}", expires_in: 12.hours) do
+        StarWarsApiService.get_vehicle(vehicle_id)
+      end
+    end
+  
     render "star_wars/show_film"
   end
+  
   
   def people
     @people = Rails.cache.fetch("all_people", expires_in: 12.hours) do
@@ -32,10 +40,23 @@ class StarWarsController < ApplicationController
 
   def show_person
     person_id = params[:id]
-    @person = StarWarsApiService.get_person(person_id)
-
+    
+    @person = Rails.cache.fetch("person_#{person_id}", expires_in: 12.hours) do
+      StarWarsApiService.get_person(person_id)
+    end
+  
+    # Fetch related films and cache the full object
+    @films = @person["films"].map do |url|
+      film_id = url.split("/").last
+      Rails.cache.fetch("film_#{film_id}", expires_in: 12.hours) do
+        StarWarsApiService.get_film(film_id)
+      end
+    end
+  
     render "star_wars/show_person"
   end
+  
+  
 
   def vehicles
     @vehicles = Rails.cache.fetch("all_vehicles", expires_in: 12.hours) do
@@ -45,8 +66,28 @@ class StarWarsController < ApplicationController
 
   def show_vehicle
     vehicle_id = params[:id]
-    @vehicle = StarWarsApiService.get_vehicle(vehicle_id)
-
+    
+    @vehicle = Rails.cache.fetch("vehicle_#{vehicle_id}", expires_in: 12.hours) do
+      StarWarsApiService.get_vehicle(vehicle_id)
+    end
+  
+    # Fetch related pilots
+    @pilots = @vehicle["pilots"].map do |url|
+      pilot_id = url.split("/").last
+      Rails.cache.fetch("person_#{pilot_id}", expires_in: 12.hours) do
+        StarWarsApiService.get_person(pilot_id)
+      end
+    end
+  
+    # Fetch related films
+    @films = @vehicle["films"].map do |url|
+      film_id = url.split("/").last
+      Rails.cache.fetch("film_#{film_id}", expires_in: 12.hours) do
+        StarWarsApiService.get_film(film_id)
+      end
+    end
+  
     render "star_wars/show_vehicle"
   end
+  
 end
